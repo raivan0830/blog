b073981b961557766dfc591d33bff0a578e47185
Jiwon Kang <raivan0830@gmail.com>
2020-10-16 21:37:11 +0900

    21.6 backend programming koa framework p.634

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,11 +1,14 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyparser = require('koa-bodyparser');
 const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
 
 router.use('/api', api.routes());
+
+app.use(bodyparser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
