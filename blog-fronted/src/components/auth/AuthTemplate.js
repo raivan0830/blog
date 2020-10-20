f7ed71fcaca5530852e84fc67291dab0ca44c353
Jiwon Kang <raivan0830@gmail.com>
2020-10-21 01:20:13 +0900

    24.2.1 prepare Templete p.740

---
+++ blog-fronted/src/components/auth/AuthTemplate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AuthTemplateBlock = styled.div``
+
+const AuthTemplate = ({ children }) => {
+    return (
+        <AuthTemplateBlock>
+            {children}
+        </AuthTemplateBlock>
+    );
+};
+
+export default AuthTemplate;
