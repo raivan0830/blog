1cc00457473af6bbd1d8697c7d53469514e599b2
Jiwon Kang <raivan0830@gmail.com>
2020-10-21 00:59:29 +0900

    24.1.5 redux,prepare p731

--- blog-fronted/src/index.js
+++ blog-fronted/src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom'
+import { createStore } from 'redux';
+import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux';
 
+const store = createStore(rootReducer, composeWithDevTools())
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
