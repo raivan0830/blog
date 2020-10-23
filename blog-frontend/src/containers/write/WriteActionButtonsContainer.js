5bb938bc57ff714184370f34be9f8735c42db1e7
Jiwon Kang <raivan0830@gmail.com>
2020-10-23 11:38:15 +0900

    25.4 writeActionButton Container p.834

---
+++ blog-frontend/src/containers/write/WriteActionButtonsContainer.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import WriteActionButtons from '../../components/write/WriteActionButtons';
+import { useDispatch, useSelector } from 'react-redux';
+import { writePost } from '../../modules/write';
+import { withRouter } from 'react-router-dom';
+
+const WriteActionButtonsContainer = ({ history }) => {
+  const dispatch = useDispatch();
+  const { title, body, tags, post, postError } = useSelector(({ write }) => ({
+    title: write.title,
+    body: write.body,
+    tags: write.tags,
+    post: write.post,
+    postError: write.postError,
+  }));
+
+  const onPublish = () => {
+    dispatch(writePost({ title, body, tags }));
+  };
+
+  const onCancel = () => {
+    history.goBack();
+  };
+
+  useEffect(() => {
+    if (post) {
+      const { _id, user } = post;
+      history.push(`/@${user.username}/${_id}`);
+    }
+    if (postError) {
+      console.log(postError);
+    }
+  }, [history, post, postError]);
+
+  return <WriteActionButtons onPublish={onPublish} onCancel={onCancel} />;
+};
+
+export default withRouter(WriteActionButtonsContainer);
